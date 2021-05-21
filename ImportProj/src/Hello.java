b788484bdd8b21a3258487e41fdde4b1211cb2e1
arunraj86 <34321772+arunraj86@users.noreply.github.com>
2021-05-21 18:34:35 -0500

    Test first commit

---
+++ ImportProj/src/Hello.java
@@ -0,0 +1,19 @@
+
+
+import org.studeasy.person.NonVegan;
+import org.studeasy.person.Person;
+import org.studeasy.person.Vegan;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		Person john = new Vegan();
+		john.breathe();
+		john.eat();
+		john.speak();
+		john.message();
+		
+
+	}
+
+}
